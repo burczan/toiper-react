644f3036aa6aff3326c61f636f9f013b53a2acf7
burczan <natalia.wajnor@gmail.com>
2021-01-23 20:17:11 +0100

    add return type of useAction hook

--- src/hooks/useAction.ts
+++ src/hooks/useAction.ts
@@ -2,7 +2,7 @@ import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actionCreators } from '../state';
 
-export const useActions = () => {
+export const useActions = (): typeof actionCreators => {
   const dispatch = useDispatch();
   return bindActionCreators(actionCreators, dispatch);
 };
