db5e8c641afb7d5a422e2c1f58a3a57b99c89f00
burczan <natalia.wajnor@gmail.com>
2021-03-09 20:23:23 +0100

    display result

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { getCheapestToiletPaper } from '../state/papers/selectors';
 import { Hero } from '../common/components/Hero';
 import { AddPaperForm } from './AddPaperForm';
 import { PapersTable } from './PapersTable';
 
 export const App: React.FC = () => {
+  const cheapest = useSelector(getCheapestToiletPaper);
+
   return (
     <>
       <Hero
@@ -11,10 +15,21 @@ export const App: React.FC = () => {
         subtitle="Compare price of toilet paper rolls depending on amount of layers and leafs"
         color="primary"
       />
-      <div className="container" style={{ maxWidth: `${window.innerWidth / 2}px` }}>
-        <div className="section">
+      <div className="container">
+        <div className="section container" style={{ maxWidth: `${window.innerWidth / 2}px` }}>
           <AddPaperForm />
         </div>
+        {cheapest && (
+          <div className="container">
+            <div className="notification is-info has-text-centered" style={{ maxWidth: '300px', margin: '0 auto' }}>
+              Paper
+              {' '}
+              <u>{cheapest && cheapest.name}</u>
+              {' '}
+              seems to be the best
+            </div>
+          </div>
+        )}
         <div className="section">
           <PapersTable />
         </div>
