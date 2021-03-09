006d62c867b5cee37dd0e9eee7128a7f5af4931a
burczan <natalia.wajnor@gmail.com>
2021-03-09 08:22:34 +0100

    change bulma theme to journal

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { App } from './components/App';
 import { store } from './state/store';
-import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import 'bulmaswatch/journal/bulmaswatch.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.render(
