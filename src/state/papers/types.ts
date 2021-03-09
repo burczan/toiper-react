ddad6caeb0eb77ac5ce2d41c54232e51581ba4d7
burczan <natalia.wajnor@gmail.com>
2021-03-09 08:30:15 +0100

    change PaperTypes to enum and add missing PaperProps

--- src/state/papers/types.ts
+++ src/state/papers/types.ts
@@ -3,9 +3,17 @@ export enum PapersActionType {
   REMOVE_PAPER = 'remove_paper',
 }
 
-export type PaperTypes = 'toilet' | 'towel';
+export enum PaperTypes {
+  'toilet' = 'toilet',
+  'towel' = 'towel',
+}
 
 export type PaperProps = {
   id: string;
   type: PaperTypes;
+  name: string;
+  price: number;
+  layers: number;
+  leafs: number;
+  layerPrice: number;
 };
