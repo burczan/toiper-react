a2b49c999ed8b74ca649c38c67c7dc96eb6fd3c6
burczan <natalia.wajnor@gmail.com>
2021-03-18 16:14:40 +0100

    add SetState and Ref types

--- src/common/components/types.ts
+++ src/common/components/types.ts
@@ -1,3 +1,7 @@
 export type Color = 'primary' | 'link' | 'info' | 'success' | 'warning' | 'danger';
 
 export type Size = 'is-small' | 'is-medium' | 'is-large' | undefined;
+
+export type SetState<State> = React.Dispatch<React.SetStateAction<State>>;
+
+export type Ref<T> = React.RefObject<T>;
