1761f4a999563687c6fddeaca61472ca5faaefc7
burczan <natalia.wajnor@gmail.com>
2021-03-18 17:06:46 +0100

    move SetState and Ref types to utils

--- src/common/components/types.ts
+++ src/common/components/types.ts
@@ -1,7 +1,3 @@
 export type Color = 'primary' | 'link' | 'info' | 'success' | 'warning' | 'danger';
 
 export type Size = 'is-small' | 'is-medium' | 'is-large' | undefined;
-
-export type SetState<State> = React.Dispatch<React.SetStateAction<State>>;
-
-export type Ref<T> = React.RefObject<T>;
--- src/common/utils/types.ts
+++ src/common/utils/types.ts
@@ -3,3 +3,7 @@ type FalsyType = false | null | undefined | '' | 0;
 export function typedBoolean<ValueType>(value: ValueType): value is Exclude<ValueType, FalsyType> {
   return Boolean(value);
 }
+
+export type SetState<State> = React.Dispatch<React.SetStateAction<State>>;
+
+export type Ref<T> = React.RefObject<T>;
